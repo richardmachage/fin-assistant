c56e29fb67345a60fdde2ec214bf37c8fb7eb8f9
Richard.Machage <richard.machage@transsion.com>
2025-03-10 11:48:05 +0300

    core: Add OutlineButtonFa

--- core/presentation/src/main/java/com/transsion/financialassistant/presentation/components/buttons/OutlineButtonFa.kt
+++ core/presentation/src/main/java/com/transsion/financialassistant/presentation/components/buttons/OutlineButtonFa.kt
@@ -1,34 +1,34 @@
 package com.transsion.financialassistant.presentation.components.buttons
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.transsion.financialassistant.presentation.theme.FAColors
-
 
 @Preview(showSystemUi = true)
 @Composable
-fun FilledButtonFa(
+fun OutlineButtonFa(
     text: String = "Save",
     onClick: () -> Unit = {},
     enabled: Boolean = true,
     modifier: Modifier = Modifier.fillMaxWidth()
 ) {
-    Button(
+    OutlinedButton(
         modifier = modifier,
         onClick = onClick,
-        colors = ButtonDefaults.buttonColors().copy(
-            containerColor = FAColors.green,
-            contentColor = Color.White
+        colors = ButtonDefaults.outlinedButtonColors().copy(
+            contentColor = FAColors.green
         ),
-        enabled = enabled
+        enabled = enabled,
+        border = BorderStroke(1.dp, FAColors.green)
     ) {
         Text(text)
     }
-}
 
+}
