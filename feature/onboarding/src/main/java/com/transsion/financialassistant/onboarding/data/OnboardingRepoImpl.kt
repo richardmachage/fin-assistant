595bc23097d9cebe580a371a3236a2cd91536f05
Richard.Machage <richard.machage@transsion.com>
2025-03-17 11:21:32 +0300

    feat(onboarding): Create Onboarding Module, Repo and RepoImpl

---
+++ feature/onboarding/src/main/java/com/transsion/financialassistant/onboarding/data/OnboardingRepoImpl.kt
@@ -0,0 +1,6 @@
+package com.transsion.financialassistant.onboarding.data
+
+import com.transsion.financialassistant.onboarding.domain.OnboardingRepo
+
+class OnboardingRepoImpl : OnboardingRepo {
+}
