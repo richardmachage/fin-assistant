2a6cbff9f5639d0301615f5450fd62206f82efbb
Richard.Machage <richard.machage@transsion.com>
2025-03-18 11:06:57 +0300

    feat(onboarding): Implement onboarding completion check and save

--- feature/onboarding/src/main/java/com/transsion/financialassistant/onboarding/data/OnboardingRepoImpl.kt
+++ feature/onboarding/src/main/java/com/transsion/financialassistant/onboarding/data/OnboardingRepoImpl.kt
@@ -1,6 +1,30 @@
 package com.transsion.financialassistant.onboarding.data
 
+import com.transsion.financialassistant.data.preferences.DatastorePreferences
+import com.transsion.financialassistant.data.preferences.SharedPreferences
 import com.transsion.financialassistant.onboarding.domain.OnboardingRepo
+import javax.inject.Inject
 
-class OnboardingRepoImpl : OnboardingRepo {
+class OnboardingRepoImpl @Inject constructor(
+    private val datastorePreferences: DatastorePreferences,
+    private val sharedPreferences: SharedPreferences
+) : OnboardingRepo {
+
+
+    override fun hasCompletedOnboarding(): Boolean {
+        //check the onboarding flag ,
+        //if empty then the it is first installation, otherwise it is not
+
+        val status = sharedPreferences.loadData(SharedPreferences.ONBOARDING_COMPLETED_KEY)
+
+        return status?.let { true } ?: false
+
+    }
+
+    override fun setCompletedOnboarding() {
+        sharedPreferences.saveData(
+            key = SharedPreferences.ONBOARDING_COMPLETED_KEY,
+            value = SharedPreferences.ONBOARDING_COMPLETED_KEY
+        )
+    }
 }
--- feature/onboarding/src/main/java/com/transsion/financialassistant/onboarding/domain/OnboardingRepo.kt
+++ feature/onboarding/src/main/java/com/transsion/financialassistant/onboarding/domain/OnboardingRepo.kt
@@ -1,4 +1,17 @@
 package com.transsion.financialassistant.onboarding.domain
 
 interface OnboardingRepo {
+
+    /**Checks if the user has completed onboarding.*/
+    fun hasCompletedOnboarding(): Boolean?
+
+
+    /** Saves the user's onboarding status.*/
+    fun setCompletedOnboarding()
+
+
+
+
+
+
 }
