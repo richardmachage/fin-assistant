81ca07483eb94212387cd31821fadba96a221ac3
Richard.Machage <richard.machage@transsion.com>
2025-04-01 10:50:00 +0300

    feat(core:presentation): Added MappingDataPoints and DataPoint for graph

---
+++ core/presentation/src/main/java/com/transsion/financialassistant/presentation/components/graphs/DataPoint.kt
@@ -0,0 +1,8 @@
+package com.transsion.financialassistant.presentation.components.graphs
+
+data class DataPoint(
+    val x: String,
+    val y: Float
+)
+
+
---
+++ core/presentation/src/main/java/com/transsion/financialassistant/presentation/components/graphs/MappingDataPoints.kt
@@ -0,0 +1,20 @@
+package com.transsion.financialassistant.presentation.components.graphs
+
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+
+fun mapToOffsets(dataPoints: List<DataPoint>, size: Size): List<Offset> {
+    val maxY = dataPoints.maxOf { it.y }
+    val minY =
+        dataPoints.minOf { it.y } // if we will ever want zero-based scaling, we just set this to 0f
+    val verticalRange = maxY - minY
+
+    val spacingX = size.width / (dataPoints.size - 1)
+
+    return dataPoints.mapIndexed { index, point ->
+        val x = index * spacingX
+        val normalizedY = (point.y - minY) / verticalRange
+        val y = size.height * (1f - normalizedY) //invert the Y axis
+        Offset(x, y)
+    }
+}
