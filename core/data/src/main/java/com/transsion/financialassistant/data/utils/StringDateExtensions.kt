0395122365ea12fca713cd7139fe794a486c571c
Richard.Machage <richard.machage@transsion.com>
2025-04-07 08:47:59 +0300

    feat(core:data): Add date conversion functions

---
+++ core/data/src/main/java/com/transsion/financialassistant/data/utils/StringDateExtensions.kt
@@ -0,0 +1,31 @@
+package com.transsion.financialassistant.data.utils
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+private val appFormatter =
+    DateTimeFormatter.ofPattern("d/M/yy", Locale.getDefault()) // e.g. 1/3/25
+private val dbFormatter = DateTimeFormatter.ISO_LOCAL_DATE // e.g. 2025-03-01
+
+fun String.toDbDate(): String? {
+    return this.let {
+        try {
+            val parsedDate = LocalDate.parse(it, appFormatter)
+            parsedDate.format(dbFormatter)
+        } catch (e: Exception) {
+            this
+        }
+    }
+}
+
+fun String.toMpesaDate(): String? {
+    return this.let {
+        try {
+            val parsedDate = LocalDate.parse(it, dbFormatter)
+            parsedDate.format(appFormatter)
+        } catch (e: Exception) {
+            this
+        }
+    }
+}
