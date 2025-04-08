c6649eab5b5ebea1caf37c271d86886f1129e17e
Richard.Machage <richard.machage@transsion.com>
2025-04-08 08:58:13 +0300

    feat(insights) : Display transaction costs for OUT transactions only

--- feature/insights/src/main/java/com/transsion/financialassistant/insights/screens/insights/InsightsScreen.kt
+++ feature/insights/src/main/java/com/transsion/financialassistant/insights/screens/insights/InsightsScreen.kt
@@ -263,16 +263,18 @@ fun InsightsScreen(
                         }
                     )
                 }
-                items(1) {
-                    InsightCategoryCard(
-                        modifier = Modifier.padding(paddingMedium),
-                        item = InsightCategoryCardItem(
-                            tittle = stringResource(R.string.transaction_costs),
-                            amount = state.totalTransactionCost ?: "0.0",
-                            categoryIcon = com.transsion.financialassistant.presentation.R.drawable.weui_arrow_outlined
-                        ),
-                        onClick = {
-                            /*navController.navigate(
+
+                if (state.transactionCategory == TransactionCategory.OUT) {
+                    items(1) {
+                        InsightCategoryCard(
+                            modifier = Modifier.padding(paddingMedium),
+                            item = InsightCategoryCardItem(
+                                tittle = stringResource(R.string.transaction_costs),
+                                amount = state.totalTransactionCost ?: "0.0",
+                                categoryIcon = com.transsion.financialassistant.presentation.R.drawable.weui_arrow_outlined
+                            ),
+                            onClick = {
+                                /*navController.navigate(
                                 InsightsRoutes.CategoryInsights(
                                     category = "transaction_costs",
                                     startDate = state.insightTimeline.getTimeline().startDate,
@@ -280,9 +282,10 @@ fun InsightsScreen(
                                     timeLine = state.insightTimeline.getTimeline().displayInfo
                                 )
                             )*/
-                        }
-                    )
+                            }
+                        )
 
+                    }
                 }
             }
         }
