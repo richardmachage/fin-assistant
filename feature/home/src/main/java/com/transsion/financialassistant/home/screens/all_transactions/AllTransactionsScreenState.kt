0177d62fdb2dc330c4af7ded5a1a9d7392f9ad52
Richard.Machage <richard.machage@transsion.com>
2025-04-11 15:27:28 +0300

    feat(home) : Remove unused fields from AllTransactionsScreenState

--- feature/home/src/main/java/com/transsion/financialassistant/home/screens/all_transactions/AllTransactionsScreenState.kt
+++ feature/home/src/main/java/com/transsion/financialassistant/home/screens/all_transactions/AllTransactionsScreenState.kt
@@ -1,15 +1,11 @@
 package com.transsion.financialassistant.home.screens.all_transactions
 
 import com.transsion.financialassistant.data.models.TransactionCategory
-import com.transsion.financialassistant.home.screens.all_transactions.model.AllTransactionsCategory
-import com.transsion.financialassistant.home.screens.all_transactions.model.AllTransactionsTimeline
 
 data class AllTransactionsScreenState(
     val isLoading: Boolean = false,
     val toastMessage: String? = null,
     val transactionCategory: TransactionCategory = TransactionCategory.IN,
-    val allTransactionsCategory: AllTransactionsCategory = AllTransactionsCategory.PERSONAL,
-    val allTransactionsTimeline: AllTransactionsTimeline = AllTransactionsTimeline.TODAY,
 
     val moneyIn: String? = null,
     val moneyOut: String? = null,
