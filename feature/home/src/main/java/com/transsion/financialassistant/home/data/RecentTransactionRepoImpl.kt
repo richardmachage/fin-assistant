dbeaf67701d3f01d7ecb208fdc90b50f29cd9675
Richard.Machage <richard.machage@transsion.com>
2025-04-14 17:26:18 +0300

    feat(home): Implement getTotalMoneyIn and getTotalMoneyOut in RecentTransactionRepoImpl

--- feature/home/src/main/java/com/transsion/financialassistant/home/data/RecentTransactionRepoImpl.kt
+++ feature/home/src/main/java/com/transsion/financialassistant/home/data/RecentTransactionRepoImpl.kt
@@ -2,19 +2,35 @@ package com.transsion.financialassistant.home.data
 
 import com.transsion.financialassistant.data.room.db.FinancialAssistantDao
 import com.transsion.financialassistant.data.room.db.UnifiedTransaction
+import com.transsion.financialassistant.data.utils.dbFormatter
 import com.transsion.financialassistant.home.domain.RecentTransactionRepo
 import kotlinx.coroutines.flow.Flow
+import java.time.LocalDate
 import javax.inject.Inject
 
 class RecentTransactionRepoImpl @Inject constructor(
     private val dao: FinancialAssistantDao
 ): RecentTransactionRepo {
+    private val todayDate = LocalDate.now().format(dbFormatter)
+
     override suspend fun getTotalMoneyIn(): Result<Double> {
-       TODO("Not yet implemented")
+        return try {
+            Result.success(
+                dao.getTotalMoneyInAmount(startDate = todayDate, endDate = todayDate) ?: 0.0
+            )
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
     }
 
     override suspend fun getTotalMoneyOut(): Result<Double> {
-        TODO("Not yet implemented")
+        return try {
+            Result.success(
+                dao.getTotalMoneyOutAmount(startDate = todayDate, endDate = todayDate) ?: 0.0
+            )
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
     }
 
     override  fun getRecentTransactions(): Flow<List<UnifiedTransaction>> {
