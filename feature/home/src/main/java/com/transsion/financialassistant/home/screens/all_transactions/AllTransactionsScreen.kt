31a0adb54d6a2f2f41345d5d24c8b7a069946565
Richard.Machage <richard.machage@transsion.com>
2025-04-15 10:49:54 +0300

    feat(home): Added AnimatedContent to switch between category filter and selected filters display. - Added filter chips to show selected filters (source and period). - Implemented a clear filter button to reset filters. - Updated UI to accommodate filter display and actions.

--- feature/home/src/main/java/com/transsion/financialassistant/home/screens/all_transactions/AllTransactionsScreen.kt
+++ feature/home/src/main/java/com/transsion/financialassistant/home/screens/all_transactions/AllTransactionsScreen.kt
@@ -1,16 +1,21 @@
 package com.transsion.financialassistant.home.screens.all_transactions
 
 import android.util.Log
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.IconButton
@@ -19,6 +24,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -29,6 +35,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
@@ -40,12 +47,15 @@ import com.transsion.financialassistant.data.utils.toAppTime
 import com.transsion.financialassistant.data.utils.toMonthDayDate
 import com.transsion.financialassistant.home.R
 import com.transsion.financialassistant.home.model.TransactionUi
+import com.transsion.financialassistant.home.screens.all_transactions.filter.FilterState
 import com.transsion.financialassistant.home.screens.all_transactions.filter.TransactionFilterDialog
+import com.transsion.financialassistant.home.screens.all_transactions.filter.isFilterEmpty
 import com.transsion.financialassistant.home.screens.components.InOutCard
 import com.transsion.financialassistant.home.screens.components.InsightCateToggleSegmentedButton
 import com.transsion.financialassistant.home.screens.components.TransactionUiListItem
 import com.transsion.financialassistant.presentation.components.buttons.IconButtonFa
 import com.transsion.financialassistant.presentation.components.texts.BigTittleText
+import com.transsion.financialassistant.presentation.components.texts.NormalText
 import com.transsion.financialassistant.presentation.theme.FAColors
 import com.transsion.financialassistant.presentation.theme.FinancialAssistantTheme
 import com.transsion.financialassistant.presentation.utils.VerticalSpacer
@@ -103,6 +113,7 @@ fun AllTransactionsScreen(
         // Filter Dialog
         if (showDialog) {
             TransactionFilterDialog(
+                filter = viewModel.filters.collectAsState().value,
                 onDismiss = { showDialog = false },
                 onApply = { filterState ->
                     // Use the applied filter state
@@ -122,45 +133,99 @@ fun AllTransactionsScreen(
                     end = paddingMedium
                 )
         ) {
-            // Money In/Out Transaction Card
-            InOutCard(
-                moneyIn = state.moneyIn ?: "0.0",
-                moneyOut = state.moneyOut ?: "0.0",
-                transactionsIn = state.transactionsIn ?: "0",
-                transactionsOut = state.transactionsOut ?: "0"
-            )
 
-            VerticalSpacer(8)
-            HorizontalDivider(
-                modifier = Modifier.fillMaxWidth()
-            )
-            VerticalSpacer(8)
+            // Money In/Out Transaction Card
+            AnimatedVisibility(visible = viewModel.filters.collectAsState().value.isFilterEmpty()) {
+                Column(
+                ) {
+                    InOutCard(
+                        moneyIn = state.moneyIn ?: "0.0",
+                        moneyOut = state.moneyOut ?: "0.0",
+                        transactionsIn = state.transactionsIn ?: "0",
+                        transactionsOut = state.transactionsOut ?: "0"
+                    )
+
+                    VerticalSpacer(8)
+                    HorizontalDivider(
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    VerticalSpacer(8)
+                }
+            }
+
+
+
             Row(
                 modifier = Modifier
                     .fillMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceEvenly
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                var selectedCategory by remember { mutableStateOf(InsightCategory.PERSONAL) }
-                InsightCateToggleSegmentedButton(
-                    modifier = Modifier.fillMaxWidth(0.8f),
-                    selectedOption = selectedCategory,
-                    onOptionSelected = {
-                        selectedCategory = it
+
+
+                AnimatedContent(targetState = viewModel.filters.collectAsState()) { filters ->
+                    when (filters.value.isFilterEmpty()) {
+                        true -> {
+                            var selectedCategory by remember { mutableStateOf(InsightCategory.PERSONAL) }
+
+                            InsightCateToggleSegmentedButton(
+                                modifier = Modifier.fillMaxWidth(0.8f),
+                                selectedOption = selectedCategory,
+                                onOptionSelected = {
+                                    selectedCategory = it
+                                }
+                            )
+                        }
+
+                        false -> {
+                            Row {
+
+                                filters.value.source?.let {
+                                    //UI
+                                    FilterItem(name = it.description)
+                                }
+
+                                filters.value.period?.let {
+                                    FilterItem(name = it.label)
+                                }
+
+                            }
+
+                        }
                     }
-                )
-                IconButtonFa(
-                    icon = painterResource(id = com.transsion.financialassistant.presentation.R.drawable.vector__1_),
-                    colors = IconButtonColors(
-                        containerColor = FAColors.lightGreen,
-                        contentColor = Color.Black,
-                        disabledContainerColor = Color.Transparent,
-                        disabledContentColor = Color.Transparent
-                    ),
-                    onClick = {
-                        showDialog = true
+
+                }
+
+
+                Row {
+                    if (viewModel.filters.collectAsState().value.isFilterEmpty().not()) {
+                        IconButtonFa(
+                            icon = Icons.Default.Clear,
+                            colors = IconButtonColors(
+                                containerColor = FAColors.lightGreen,
+                                contentColor = Color.Black,
+                                disabledContainerColor = Color.Transparent,
+                                disabledContentColor = Color.Transparent
+                            ),
+                            onClick = {
+                                viewModel.onChangeFilters(filterState = FilterState())
+                            }
+                        )
                     }
-                )
+
+                    IconButtonFa(
+                        icon = painterResource(id = com.transsion.financialassistant.presentation.R.drawable.vector__1_),
+                        colors = IconButtonColors(
+                            containerColor = FAColors.lightGreen,
+                            contentColor = Color.Black,
+                            disabledContainerColor = Color.Transparent,
+                            disabledContentColor = Color.Transparent
+                        ),
+                        onClick = {
+                            showDialog = true
+                        }
+                    )
+                }
             }
             VerticalSpacer(16)
 
@@ -210,4 +275,34 @@ fun AllTransactionScreenPreview() {
     FinancialAssistantTheme {
         AllTransactionsScreen(navController = rememberNavController())
     }
+}
+
+
+@Composable
+private fun FilterItem(name: String, onClick: () -> Unit = {}) {
+    Box(
+        modifier = Modifier
+            .padding(paddingSmall)
+            .background(color = FAColors.lightGreen, shape = RoundedCornerShape(50)),
+        contentAlignment = Alignment.Center
+
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            NormalText(
+                modifier = Modifier.padding(paddingMedium),
+                text = name,
+                fontSize = 16.sp,
+                textColor = Color.Black
+            )
+            /*IconButton(
+                onClick = onClick
+            ) {
+                Icon(imageVector = Icons.Default.Clear, contentDescription = "clear")
+            }*/
+
+        }
+    }
+
 }
