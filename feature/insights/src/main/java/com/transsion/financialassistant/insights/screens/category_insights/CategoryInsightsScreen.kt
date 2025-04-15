63c682050397fe42e74f38f57bb7bee7623b0c32
musyokijoxhua <musyokijoxhua@gmail.com>
2025-04-15 15:54:28 +0300

    feat(insights): Use AutoMirrored ArrowBack icon in CategoryInsightsScreen
    - Replaced `Icons.Default.ArrowBack` with `Icons.AutoMirrored.Filled.ArrowBack` in `CategoryInsightsScreen`.
    - This change uses an icon that automatically mirrors based on the layout direction.

--- feature/insights/src/main/java/com/transsion/financialassistant/insights/screens/category_insights/CategoryInsightsScreen.kt
+++ feature/insights/src/main/java/com/transsion/financialassistant/insights/screens/category_insights/CategoryInsightsScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -46,7 +47,7 @@ fun CategoryInsightsScreen(
                         onClick = { navController.navigateUp() }
                     ) {
                         Icon(
-                            imageVector = Icons.Default.ArrowBack,
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                             contentDescription = "back"
                         )
                     }
