36509456a2f56cf867b1d55f0b224c2fda81bb2d
Richard.Machage <richard.machage@transsion.com>
2025-04-16 16:15:00 +0300

    feat(Onboarding): Update navigation and survey completion logic

--- feature/onboarding/src/main/java/com/transsion/financialassistant/onboarding/navigation/OnboardingNavGraph.kt
+++ feature/onboarding/src/main/java/com/transsion/financialassistant/onboarding/navigation/OnboardingNavGraph.kt
@@ -5,7 +5,6 @@ import androidx.annotation.RequiresApi
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import com.transsion.financialassistant.home.screens.HomeScreen
 import com.transsion.financialassistant.onboarding.screens.change_number.ChangePhoneNumberInstructions
 import com.transsion.financialassistant.onboarding.screens.confirm_number.ConfirmNumberDualScreen
 import com.transsion.financialassistant.onboarding.screens.create_pin.CreatePinScreen
@@ -72,10 +71,6 @@ fun NavGraphBuilder.onboardingNavGraph(
             navController = navController,
             goToLanding = goToLanding
         )
-    }
-
-    composable<OnboardingRoutes.HomeScreen> {
-        HomeScreen()
     }
 
 
