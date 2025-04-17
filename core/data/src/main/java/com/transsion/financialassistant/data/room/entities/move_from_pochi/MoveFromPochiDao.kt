8aeee8a473afca23099be7d92ed32ff3fa457746
Richard.Machage <richard.machage@transsion.com>
2025-04-17 10:41:40 +0300

    feat(core:data): Add MoveFromPochi Entity and Dao

---
+++ core/data/src/main/java/com/transsion/financialassistant/data/room/entities/move_from_pochi/MoveFromPochiDao.kt
@@ -0,0 +1,32 @@
+package com.transsion.financialassistant.data.room.entities.move_from_pochi
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface MoveFromPochiDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(moveToPochiEntity: MoveFromPochiEntity)
+
+    @Query("SELECT * FROM MoveFromPochiEntity")
+    fun getAll(): Flow<List<MoveFromPochiEntity>>
+
+    @Query("SELECT * FROM MoveFromPochiEntity WHERE transactionCode = :transactionCode")
+    fun getByTransactionCode(transactionCode: String): MoveFromPochiEntity?
+
+    @Update
+    suspend fun updateRecord(moveToPochiEntity: MoveFromPochiEntity)
+
+    @Delete
+    suspend fun deleteRecord(moveToPochiEntity: MoveFromPochiEntity)
+
+    @Query("SELECT * FROM MoveFromPochiEntity WHERE date BETWEEN :startDate AND :endDate ORDER BY date ASC")
+    suspend fun getRecordsByDate(startDate: String, endDate: String): List<MoveFromPochiEntity>
+
+}
