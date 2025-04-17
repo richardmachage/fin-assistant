b42001ea674e001aab10fbe9cfef977fdf940563
Richard.Machage <richard.machage@transsion.com>
2025-04-17 11:02:39 +0300

    feat(core:data): Add transactionType and transactionCategory to SendFromPochiEntity

--- core/data/src/main/java/com/transsion/financialassistant/data/room/entities/send_from_pochi/SendFromPochiEntity.kt
+++ core/data/src/main/java/com/transsion/financialassistant/data/room/entities/send_from_pochi/SendFromPochiEntity.kt
@@ -2,6 +2,8 @@ package com.transsion.financialassistant.data.room.entities.send_from_pochi
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.transsion.financialassistant.data.models.TransactionCategory
+import com.transsion.financialassistant.data.models.TransactionType
 
 
 @Entity
@@ -14,5 +16,7 @@ data class SendFromPochiEntity(
     val phone: String,
     val businessBalance: Double,
     val sentToName: String,
-    val transactionCost: Double
+    val transactionCost: Double,
+    val transactionType: TransactionType = TransactionType.SEND_MONEY_FROM_POCHI,
+    val transactionCategory: TransactionCategory = TransactionCategory.OUT
 )
