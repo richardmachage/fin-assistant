bdde4c89b4676b25349dbe765acc5da72e30b7e7
Richard.Machage <richard.machage@transsion.com>
2025-04-30 17:05:09 +0300

    feat: Added Google Firebase App Distribution and Google GMS Services plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,6 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.google.firebase.appdistribution) apply false
+    alias(libs.plugins.google.gms.google.services) apply false
 }
