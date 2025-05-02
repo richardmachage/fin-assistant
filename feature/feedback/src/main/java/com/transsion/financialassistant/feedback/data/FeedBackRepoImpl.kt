8188fb8bfb77e28a979516524c596c5c5b3c4686
Richard.Machage <richard.machage@transsion.com>
2025-05-02 15:03:23 +0300

    feat(feedback): successfully implemented sending of feedback to remote firebase

--- feature/feedback/src/main/java/com/transsion/financialassistant/feedback/data/FeedBackRepoImpl.kt
+++ feature/feedback/src/main/java/com/transsion/financialassistant/feedback/data/FeedBackRepoImpl.kt
@@ -1,23 +1,37 @@
 package com.transsion.financialassistant.feedback.data
 
+import com.google.firebase.firestore.FirebaseFirestore
+import com.transsion.financialassistant.feedback.FEEDBACK_COLLECTION
 import com.transsion.financialassistant.feedback.domain.FeedBackRepo
 import com.transsion.financialassistant.feedback.model.FeedBack
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
-class FeedBackRepoImpl @Inject constructor() : FeedBackRepo {
+class FeedBackRepoImpl @Inject constructor(
+    private val firestore: FirebaseFirestore
+) : FeedBackRepo {
     override suspend fun sendFeedback(
         feedback: FeedBack,
         onSuccess: () -> Unit,
         onError: (String) -> Unit
     ) {
-        delay(2200)
 
-        if (feedback.title.lowercase() == "error") {
+        firestore.collection(FEEDBACK_COLLECTION)
+            .add(feedback)
+            .addOnSuccessListener {
+                onSuccess()
+            }
+            .addOnFailureListener {
+                onError(it.message.toString())
+            }
+            .await()
+
+
+        /*if (feedback.title.lowercase() == "error") {
             onError("Failed to send feedback")
         } else {
             onSuccess()
-        }
+        }*/
     }
 
 }
