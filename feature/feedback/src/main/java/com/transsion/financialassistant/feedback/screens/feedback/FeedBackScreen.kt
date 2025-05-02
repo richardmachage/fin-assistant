4ce5072849701590c59e7bc6820ff64158f49c66
Richard.Machage <richard.machage@transsion.com>
2025-05-02 12:51:44 +0300

    feat(feedback): Implement Feedback Screen

--- feature/feedback/src/main/java/com/transsion/financialassistant/feedback/screens/feedback/FeedBackScreen.kt
+++ feature/feedback/src/main/java/com/transsion/financialassistant/feedback/screens/feedback/FeedBackScreen.kt
@@ -1,4 +1,127 @@
 package com.transsion.financialassistant.feedback.screens.feedback
 
-class FeedBackScreen {
+import android.graphics.BitmapFactory
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.transsion.financialassistant.feedback.R
+import com.transsion.financialassistant.presentation.components.buttons.FilledButtonFa
+import com.transsion.financialassistant.presentation.components.buttons.OutlineButtonFa
+import com.transsion.financialassistant.presentation.components.texts.BigTittleText
+import com.transsion.financialassistant.presentation.theme.FinancialAssistantTheme
+
+
+@Composable
+fun FeedBackScreen() {
+
+    val context = LocalContext.current
+    var title by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
+    var attachment by remember { mutableStateOf<ImageBitmap?>(null) }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri ->
+        uri?.let {
+            val stream = context.contentResolver.openInputStream(it)
+            val bitmap = BitmapFactory.decodeStream(stream)
+            attachment = bitmap.asImageBitmap()
+        }
+    }
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        //Text(text = stringResource(R.string.submit_feedback), style = MaterialTheme.typography.headlineSmall)
+        BigTittleText(text = stringResource(R.string.submit_feedback))
+        OutlinedTextField(
+            value = title,
+            onValueChange = { title = it },
+            label = { Text(stringResource(R.string.title)) },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = description,
+            onValueChange = { description = it },
+            label = { Text(stringResource(R.string.description)) },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(150.dp),
+            maxLines = 10
+        )
+
+        //attachment
+        if (attachment != null) {
+            Image(
+                bitmap = attachment!!,
+                contentDescription = "Screenshot",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .clip(RoundedCornerShape(12.dp))
+                    .border(1.dp, Color.Gray)
+            )
+        }
+
+
+        OutlineButtonFa(
+            text = stringResource(R.string.attach_screenshot),
+            onClick = { launcher.launch("image/*") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+
+
+        FilledButtonFa(
+            enabled = title.isNotBlank() && description.isNotBlank(),
+            text = stringResource(R.string.submit),
+            onClick = {
+                //TODO
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@PreviewLightDark
+@Composable
+fun FeedBackPrev() {
+    FinancialAssistantTheme {
+        FeedBackScreen()
+    }
 }
