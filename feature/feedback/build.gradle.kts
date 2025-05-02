6c38d824d66bfcacce8ad06eaa19c26bf3baa398
Richard.Machage <richard.machage@transsion.com>
2025-05-02 12:14:54 +0300

    feat(feature:feedback): Added a new `:feature:feedback` module. `

--- feature/feedback/build.gradle.kts
+++ feature/feedback/build.gradle.kts
@@ -1,10 +1,9 @@
 plugins {
     alias(libs.plugins.financialassistant.feature.module)
-
 }
 
 android {
-    namespace = "com.transsion.financialassistant.home"
+    namespace = "com.transsion.financialassistant.feedback"
 }
 
 dependencies {
@@ -12,17 +11,21 @@ dependencies {
     implementation(projects.core.presentation)
     implementation(projects.core.data)
 
+
+
     implementation(libs.androidx.compose.material)
     implementation(libs.androidx.rules)
 
-    //paging
-    implementation(libs.androidx.paging.common.android)
-    implementation(libs.androidx.paging.compose)
-
+    /*implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)*/
 
     debugImplementation(libs.ui.tooling)
 
+
     //data store
     implementation(libs.datastore.preferences)
-
 }
