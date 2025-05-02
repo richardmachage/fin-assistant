79bc43150c7ad76bd5916c095509129c25b47ca3
Richard.Machage <richard.machage@transsion.com>
2025-05-02 14:30:00 +0300

    feat(feedback): Set up FeedbackModule for dependency injection

--- feature/feedback/src/main/java/com/transsion/financialassistant/feedback/di/FeedbackModule.kt
+++ feature/feedback/src/main/java/com/transsion/financialassistant/feedback/di/FeedbackModule.kt
@@ -3,6 +3,12 @@ package com.transsion.financialassistant.feedback.di
 import com.transsion.financialassistant.feedback.data.FeedBackRepoImpl
 import com.transsion.financialassistant.feedback.domain.FeedBackRepo
 import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
 
 abstract class FeedbackModule {
 
