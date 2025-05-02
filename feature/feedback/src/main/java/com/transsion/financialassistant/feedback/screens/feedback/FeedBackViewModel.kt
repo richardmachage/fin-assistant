e124c99b44176a9a3728ada188062369baa39edc
Richard.Machage <richard.machage@transsion.com>
2025-05-02 14:31:05 +0300

    feature(feedback): Implement Feedback Screen with title, description, and attachment

--- feature/feedback/src/main/java/com/transsion/financialassistant/feedback/screens/feedback/FeedBackViewModel.kt
+++ feature/feedback/src/main/java/com/transsion/financialassistant/feedback/screens/feedback/FeedBackViewModel.kt
@@ -1,4 +1,85 @@
 package com.transsion.financialassistant.feedback.screens.feedback
 
-class FeedBackViewModel {
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.transsion.financialassistant.feedback.domain.FeedBackRepo
+import com.transsion.financialassistant.feedback.model.FeedBack
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class FeedBackViewModel @Inject constructor(
+    private val feedBackRepo: FeedBackRepo
+) : ViewModel() {
+    private var _state = MutableStateFlow(FeedbackScreenState())
+    val state = _state.asStateFlow()
+
+    fun onTittleChange(title: String) {
+        _state.update { it.copy(title = title) }
+    }
+
+    fun onDescriptionChange(description: String) {
+        _state.update { it.copy(description = description) }
+    }
+
+    fun onAttachmentChange(attachment: ImageBitmap?) {
+        _state.update { it.copy(attachment = attachment) }
+    }
+
+    fun onSubmitFeedback(
+        onSuccess: () -> Unit,
+        onError: (String) -> Unit
+    ) {
+
+        viewModelScope.launch {
+            isLoading(true)
+            feedBackRepo.sendFeedback(
+                feedback = FeedBack(
+                    title = state.value.title,
+                    description = state.value.description
+                ),
+                onSuccess = {
+                    clearFields()
+                    isLoading(false)
+                    showToast(message = "Feedback sent successfully")
+                    onSuccess()
+                },
+                onError = {
+                    isLoading(false)
+                    showToast(message = "Error: $it")
+                    onError(it)
+                }
+            )
+        }
+
+    }
+
+    private fun isLoading(status: Boolean) {
+        _state.update { it.copy(isLoading = status) }
+    }
+
+    private fun clearFields() {
+        _state.update {
+            it.copy(
+                title = "",
+                description = "",
+                attachment = null
+            )
+        }
+    }
+
+    fun showToast(message: String) {
+        _state.update { it.copy(toastMessage = message) }
+    }
+
+    fun clearToast() {
+        _state.update { it.copy(toastMessage = null) }
+    }
+
+
 }
--- feature/feedback/src/main/java/com/transsion/financialassistant/feedback/screens/feedback/FeedbackScreenState.kt
+++ feature/feedback/src/main/java/com/transsion/financialassistant/feedback/screens/feedback/FeedbackScreenState.kt
@@ -1,5 +1,11 @@
 package com.transsion.financialassistant.feedback.screens.feedback
 
+import androidx.compose.ui.graphics.ImageBitmap
+
 data class FeedbackScreenState(
     val isLoading: Boolean = false,
+    val title: String = "",
+    val description: String = "",
+    val attachment: ImageBitmap? = null,
+    val toastMessage: String? = null
 )
