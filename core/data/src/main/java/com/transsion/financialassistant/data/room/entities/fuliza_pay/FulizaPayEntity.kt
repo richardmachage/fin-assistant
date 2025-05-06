9f350315303dc5e8b21215b50f632d4770799df7
Richard.Machage <richard.machage@transsion.com>
2025-05-06 17:00:26 +0300

    feat(core:data) : Add phone to FulizaPayEntity

--- core/data/src/main/java/com/transsion/financialassistant/data/room/entities/fuliza_pay/FulizaPayEntity.kt
+++ core/data/src/main/java/com/transsion/financialassistant/data/room/entities/fuliza_pay/FulizaPayEntity.kt
@@ -3,6 +3,7 @@ package com.transsion.financialassistant.data.room.entities.fuliza_pay
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.transsion.financialassistant.data.models.TransactionCategory
+import com.transsion.financialassistant.data.models.TransactionType
 
 @Entity
 data class FulizaPayEntity(
@@ -10,11 +11,11 @@ data class FulizaPayEntity(
     val transactionCode: String,
     val amount: Double,
     val availableFulizaLimit: Double,
+    val phone: String,
     val mpesaBalance: Double,
     val transactionCategory: TransactionCategory = TransactionCategory.OUT,
     val date: String,
     val time: String,
-    val transactionCost: Double,
-    //val transactionType: TransactionType = TransactionType.PAY_BILL
+    val transactionType: TransactionType = TransactionType.FULIZA_PAY
 
 )
