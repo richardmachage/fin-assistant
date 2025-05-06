17f3f43a35b5fb1d3d492410631606fbf20b84b7
Richard.Machage <richard.machage@transsion.com>
2025-05-06 16:59:26 +0300

    refactor(core:data) : Update FulizaPayDao return types

--- core/data/src/main/java/com/transsion/financialassistant/data/room/entities/fuliza_pay/FulizaPayDao.kt
+++ core/data/src/main/java/com/transsion/financialassistant/data/room/entities/fuliza_pay/FulizaPayDao.kt
@@ -6,7 +6,6 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
-import com.transsion.financialassistant.data.room.entities.paybill_till.PayBillEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -21,11 +20,11 @@ interface FulizaPayDao {
     suspend fun delete(fulizaPayEntity: FulizaPayEntity)
 
     @Query("SELECT * FROM FulizaPayEntity")
-    fun getAll(): Flow<List<PayBillEntity>>
+    fun getAll(): Flow<List<FulizaPayEntity>>
 
     @Query("SELECT * FROM FulizaPayEntity WHERE date BETWEEN :startDate AND :endDate ORDER BY date ASC")
     suspend fun getPayFulizaTransactionsByDate(
         startDate: String,
         endDate: String
-    ): List<PayBillEntity>
+    ): List<FulizaPayEntity>
 }
