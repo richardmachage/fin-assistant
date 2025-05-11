922217956a33a06ddbda1f987d78c6156f88f59a
Richard.Machage <richard.machage@transsion.com>
2025-05-11 12:32:01 +0300

    feat(feature:admin): Integrate navigation graph with AdminMainActivity

--- feature/admin/src/main/java/com/transsion/financialassistant/admin/screens/AdminMainActivity.kt
+++ feature/admin/src/main/java/com/transsion/financialassistant/admin/screens/AdminMainActivity.kt
@@ -4,31 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import com.transsion.financialassistant.admin.screens.ui.theme.FinancialAssistantTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.transsion.financialassistant.admin.navigation.AdminRoutes
+import com.transsion.financialassistant.admin.navigation.adminNavGraph
+import com.transsion.financialassistant.presentation.theme.FinancialAssistantTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AdminMainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
             FinancialAssistantTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Box(
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .fillMaxSize(),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text("Welcome to admin!")
-                    }
-
+                NavHost(
+                    navController = navController,
+                    startDestination = AdminRoutes.Home
+                ) {
+                    adminNavGraph(navController)
                 }
             }
         }
