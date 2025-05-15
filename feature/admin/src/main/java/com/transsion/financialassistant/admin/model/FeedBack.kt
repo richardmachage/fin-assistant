e7c22b190d20ee65b932c6e6676ec5628166a014
Richard.Machage <richard.machage@transsion.com>
2025-05-15 17:02:49 +0300

    feat(feature:admin) : Add real time listener for Feedback
    Introduced a real-time listener for fetching feedback data from Firestore in the Admin feature.
    Added a timestamp field to the FeedBack data class in both admin and feedback features.
    The listener is registered in the `init` block of `HomeViewModel` and removed when the ViewModel is cleared.

--- feature/admin/src/main/java/com/transsion/financialassistant/admin/model/FeedBack.kt
+++ feature/admin/src/main/java/com/transsion/financialassistant/admin/model/FeedBack.kt
@@ -4,5 +4,6 @@ package com.transsion.financialassistant.admin.model
 data class FeedBack(
     val title: String = "",
     val description: String = "",
-    val attachment: String? = null
+    val attachment: String? = null,
+    val timestamp: Long = System.currentTimeMillis(),
 )
