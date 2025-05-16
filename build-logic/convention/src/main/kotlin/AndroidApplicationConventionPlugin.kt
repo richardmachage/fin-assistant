5c1dad1de750c1f4f0b0346f60f3b47c03b3a26b
Richard.Machage <richard.machage@transsion.com>
2025-05-16 16:21:01 +0300

    build(convention): Update app version to 1.0.1dev-test

--- build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -25,7 +25,7 @@ class AndroidApplicationConventionPlugin : Plugin<Project>{
                     minSdk = 26
                     targetSdk = 35
                     versionCode = 1
-                    versionName = "1.0"
+                    versionName = "1.0.1dev-test"
 
                     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
                 }
