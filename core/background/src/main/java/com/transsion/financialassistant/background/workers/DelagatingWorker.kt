bd258c6a2ceb1aab3dcf2611a94be81af42bc665
Richard.Machage <richard.machage@transsion.com>
2025-05-16 16:20:47 +0300

    feat(core:data): Add message parsing metrics

--- core/background/src/main/java/com/transsion/financialassistant/background/workers/DelagatingWorker.kt
+++ core/background/src/main/java/com/transsion/financialassistant/background/workers/DelagatingWorker.kt
@@ -1,6 +1,8 @@
 package com.transsion.financialassistant.background.workers
 
 import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
@@ -14,3 +16,10 @@ for use in tests to instanciate the worker
 interface HiltWorkerFactoryEntryPoint {
     fun hiltWorkerFactory(): HiltWorkerFactory
 }
+
+val constraints =
+    androidx.work.Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
+val workRequest = OneTimeWorkRequestBuilder<UnknownUploadsWorker>()
+    .setConstraints(constraints)
+    .build()
+
