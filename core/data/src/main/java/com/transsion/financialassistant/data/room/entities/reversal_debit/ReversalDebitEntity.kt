a3c056a376b507556aa857bb10d2a52fd4a15866
Richard.Machage <richard.machage@transsion.com>
2025-05-20 16:29:53 +0300

    feat(core:data): Add ReversalDebitEntity and ReversalDebitDao
    This commit introduces the ReversalDebitEntity and ReversalDebitDao to handle ReversalDebit transactions.
    
    The ReversalDebitEntity has been updated to use `transactionCode` as the primary key and the `data` field has been renamed to `date`. It also now includes a `transactionType` field defaulted to `REVERSAL_DEBIT`.
    
    The ReversalDebitDao has been updated to include a method `getReversalDebitTransactionsByDate` for retrieving transactions within a specified date range. The `getAllPaged` method has been removed.

--- core/data/src/main/java/com/transsion/financialassistant/data/room/entities/reversal_debit/ReversalDebitEntity.kt
+++ core/data/src/main/java/com/transsion/financialassistant/data/room/entities/reversal_debit/ReversalDebitEntity.kt
@@ -3,18 +3,19 @@ package com.transsion.financialassistant.data.room.entities.reversal_debit
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.transsion.financialassistant.data.models.TransactionCategory
+import com.transsion.financialassistant.data.models.TransactionType
 
 @Entity
 data class ReversalDebitEntity(
     @PrimaryKey
-    val transactionCost: String,
+    val transactionCode: String,
     val phone: String,
     val transactionReversedCode: String,
     val amount: Double,
     val mpesaBalance: Double,
-    val data: String,
+    val date: String,
     val time: String,
-    //val transactionType : TransactionType. ,
+    val transactionType: TransactionType = TransactionType.REVERSAL_DEBIT,
     val transactionCategory: TransactionCategory = TransactionCategory.OUT,
 
     )
