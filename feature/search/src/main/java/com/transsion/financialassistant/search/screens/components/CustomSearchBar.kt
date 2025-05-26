bb6a7df4459a85fb3de85ab43a2ff7ba3770c97a
Richard.Machage <richard.machage@transsion.com>
2025-05-26 12:43:00 +0300

    feat(feature:search): Implement custom search bar

---
+++ feature/search/src/main/java/com/transsion/financialassistant/search/screens/components/CustomSearchBar.kt
@@ -0,0 +1,80 @@
+package com.transsion.financialassistant.search.screens.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.tooling.preview.Preview
+import com.transsion.financialassistant.presentation.theme.FinancialAssistantTheme
+
+@Composable
+fun CustomSearchBar(
+    modifier: Modifier = Modifier,
+    query: String,
+    onQueryChanged: (String) -> Unit,
+    onFocusChanged: (Boolean) -> Unit,
+) {
+
+    var isFocused by remember { mutableStateOf(false) }
+
+    OutlinedTextField(
+        modifier = modifier
+            .onFocusChanged { focusState ->
+                val nowFocused = focusState.isFocused
+                if (nowFocused != isFocused) {
+                    onFocusChanged(nowFocused)
+                    isFocused = nowFocused
+                }
+            },
+        value = query,
+        onValueChange = onQueryChanged,
+        placeholder = { Text("Search by name, code, type") },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "Search Icon"
+            )
+        },
+        trailingIcon = {
+            if (query.isNotEmpty()) {
+                IconButton(
+                    onClick = {
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Clear,
+                        contentDescription = "Search Icon"
+                    )
+                }
+            }
+        },
+        singleLine = true,
+        shape = RoundedCornerShape(50)
+    )
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun CustomSearchBarPreview() {
+    FinancialAssistantTheme {
+        CustomSearchBar(
+            modifier = Modifier.fillMaxWidth(),
+            query = "",
+            onQueryChanged = {},
+            onFocusChanged = {},
+        )
+    }
+}
