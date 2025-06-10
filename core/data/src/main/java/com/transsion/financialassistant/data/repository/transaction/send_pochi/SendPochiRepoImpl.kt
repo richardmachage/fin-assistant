6824588de4c68d3787cd3dccc38c8fa39017128e
Richard.Machage <richard.machage@transsion.com>
2025-06-10 09:47:46 +0300

    feat(core:data) : Record `Send Pochi` to `Ziidi` as `Send Money`

--- core/data/src/main/java/com/transsion/financialassistant/data/repository/transaction/send_pochi/SendPochiRepoImpl.kt
+++ core/data/src/main/java/com/transsion/financialassistant/data/repository/transaction/send_pochi/SendPochiRepoImpl.kt
@@ -5,11 +5,14 @@ import android.content.Context
 import androidx.annotation.RequiresPermission
 import com.transsion.financialassistant.data.repository.getReceivingAddress
 import com.transsion.financialassistant.data.repository.transaction.TransactionRepoImpl
+import com.transsion.financialassistant.data.room.entities.send_money.SendMoneyDao
+import com.transsion.financialassistant.data.room.entities.send_money.SendMoneyEntity
 import com.transsion.financialassistant.data.room.entities.send_pochi.SendPochiDao
 import javax.inject.Inject
 
 class SendPochiRepoImpl @Inject constructor(
-    private val sendPochiDao: SendPochiDao
+    private val sendPochiDao: SendPochiDao,
+    private val sendMoneyDao: SendMoneyDao,
 ): SendPochiRepo, TransactionRepoImpl() {
     @RequiresPermission(anyOf = [Manifest.permission.READ_PHONE_NUMBERS, "carrier privileges", "android.permission.READ_PRIVILEGED_PHONE_STATE"])
     override suspend fun insertSendPochiTransaction(
@@ -33,10 +36,30 @@ class SendPochiRepoImpl @Inject constructor(
 
             // insert transaction to db
             sendPochiEntity?.let {
-                sendPochiDao.insert(it)
+                if (it.sentToName.lowercase() == "ziidi") {
+                    //insert into db as send Money
+                    sendMoneyDao.insert(
+                        SendMoneyEntity(
+                            transactionCode = it.transactionCode,
+                            phone = it.phone,
+                            sentToName = it.sentToName,
+                            sentToPhone = it.sentToName,
+                            amount = it.amount,
+                            mpesaBalance = it.mpesaBalance,
+                            transactionCost = it.transactionCost,
+                            date = it.date,
+                            time = it.time
+                        )
+                    )
+
+                } else {
+                    sendPochiDao.insert(it)
+                }
                 onSuccess()
             }?: run {
-                // handle insert data into db errors
+
+
+            // handle insert data into db errors
                 return onFailure("Failed to parse send pochi message")
             }
 
