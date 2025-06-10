995a1aec389aabb9a6e0ad0295baf942950ea6f9
Richard.Machage <richard.machage@transsion.com>
2025-06-10 14:31:45 +0300

    feat(app): Add settings module
    This commit adds the `settings` feature module as a dependency to the main app module. It also includes the `build.gradle.kts` file for the new `settings` module, defining its namespace and dependencies.

---
+++ feature/settings/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    alias(libs.plugins.financialassistant.feature.module)
+}
+
+android {
+    namespace = "com.transsion.financialassistant.settings"
+}
+
+dependencies {
+
+    //project dependencies
+    //implementation(projects.core.presentation)
+    implementation(projects.core.data)
+    implementation(projects.core.presentation)
+
+    implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.rules)
+
+    //paging
+    implementation(libs.androidx.paging.common.android)
+    implementation(libs.androidx.paging.compose)
+
+    debugImplementation(libs.ui.tooling)
+
+    //data store
+    implementation(libs.datastore.preferences)
+}
