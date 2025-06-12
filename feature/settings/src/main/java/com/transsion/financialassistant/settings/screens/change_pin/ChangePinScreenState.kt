84c521a7436df3a4853706b1bde70c8d5e117beb
Richard.Machage <richard.machage@transsion.com>
2025-06-12 11:21:15 +0300

    feature(settings) : Add Change Pin screen and view model

---
+++ feature/settings/src/main/java/com/transsion/financialassistant/settings/screens/change_pin/ChangePinScreenState.kt
@@ -0,0 +1,7 @@
+package com.transsion.financialassistant.settings.screens.change_pin
+
+data class ChangePinScreenState(
+    val isLoading: Boolean = false,
+    val oldPin: String = "",
+    val newPin: String = "",
+)
---
+++ feature/settings/src/main/java/com/transsion/financialassistant/settings/screens/change_pin/ChangePinViewModel.kt
@@ -0,0 +1,10 @@
+package com.transsion.financialassistant.settings.screens.change_pin
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ChangePinViewModel @Inject constructor() : ViewModel() {
+
+}
