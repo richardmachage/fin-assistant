1133e3e32a97b245a5f82d14017e74b3f9d30b25
Richard.Machage <richard.machage@transsion.com>
2025-06-16 15:35:14 +0300

    feat(settings): Add core-background, work manager, and hilt-work dependencies

--- feature/settings/build.gradle.kts
+++ feature/settings/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation(projects.core.data)
     implementation(projects.core.presentation)
     implementation(projects.feature.feedback)
+    implementation(projects.core.background)
 
     implementation(libs.androidx.compose.material)
     implementation(libs.androidx.rules)
@@ -25,4 +26,9 @@ dependencies {
 
     //data store
     implementation(libs.datastore.preferences)
+
+    //worker
+    implementation(libs.androidx.work.ktx)
+    implementation(libs.hilt.work)
+    implementation(libs.androidx.appcompat)
 }
