efdc0f7585de97fd852c0263a847f8ca25b431b5
Richard.Machage <richard.machage@transsion.com>
2025-06-16 15:33:26 +0300

    feat(settings): Add `SettingScreenState`

---
+++ feature/settings/src/main/java/com/transsion/financialassistant/settings/screens/settings/SettingScreenState.kt
@@ -0,0 +1,6 @@
+package com.transsion.financialassistant.settings.screens.settings
+
+data class SettingScreenState(
+    val showRefreshLoading: Boolean = false,
+    val showSyncAllLoading: Boolean = false
+)
