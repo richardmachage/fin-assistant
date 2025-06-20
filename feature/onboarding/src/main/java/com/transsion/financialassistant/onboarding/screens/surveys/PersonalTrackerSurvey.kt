db27df6b2ad89246b9f82738e283502a14b480b6
Richard.Machage <richard.machage@transsion.com>
2025-06-20 12:26:17 +0300

    removed

--- feature/onboarding/src/main/java/com/transsion/financialassistant/onboarding/screens/surveys/PersonalTrackerSurvey.kt
+++ feature/onboarding/src/main/java/com/transsion/financialassistant/onboarding/screens/surveys/PersonalTrackerSurvey.kt
@@ -1,54 +1,6 @@
 package com.transsion.financialassistant.onboarding.screens.surveys
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.navigationBars
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.FilterChip
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
-import com.transsion.financialassistant.onboarding.R
-import com.transsion.financialassistant.onboarding.navigation.OnboardingRoutes
-import com.transsion.financialassistant.presentation.R.drawable
-import com.transsion.financialassistant.presentation.components.buttons.FilledButtonFa
-import com.transsion.financialassistant.presentation.components.texts.BigTittleText
-import com.transsion.financialassistant.presentation.components.texts.FaintText
-import com.transsion.financialassistant.presentation.components.texts.NormalText
-import com.transsion.financialassistant.presentation.theme.FinancialAssistantTheme
-import com.transsion.financialassistant.presentation.utils.HorizontalSpacer
-import com.transsion.financialassistant.presentation.utils.VerticalSpacer
-import com.transsion.financialassistant.presentation.utils.paddingMedium
-import com.transsion.financialassistant.presentation.utils.paddingSmall
-
+/*
 
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
@@ -177,4 +129,4 @@ fun PersonalTrackerSurveyPreview(){
     FinancialAssistantTheme {
       // PersonalTrackerSurvey()
     }
-}
+}*/
