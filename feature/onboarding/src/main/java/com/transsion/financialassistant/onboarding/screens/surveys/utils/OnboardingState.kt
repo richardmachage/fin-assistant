73fcb5c88f209806601b00b1ca8d6d51fad7c7aa
Richard.Machage <richard.machage@transsion.com>
2025-06-20 12:29:32 +0300

    Refactor(onboarding): Comment out survey screens and related code

--- feature/onboarding/src/main/java/com/transsion/financialassistant/onboarding/screens/surveys/utils/OnboardingState.kt
+++ feature/onboarding/src/main/java/com/transsion/financialassistant/onboarding/screens/surveys/utils/OnboardingState.kt
@@ -1,10 +1,10 @@
 package com.transsion.financialassistant.onboarding.screens.surveys.utils
 
-data class OnboardingState(
+/*data class OnboardingState(
     val purpose: String = "",
     val personalExpenses: String = "",
     val businessType: String = "",
     val businessExpenses: List<String> = emptyList(),
     val paymentMethod: String = "",
     val isOnboardingCompleted: Boolean = false
-)
+)*/
