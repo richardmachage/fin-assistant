013f18118a6e08dac0c9ea609f1958a90881a617
Richard.Machage <richard.machage@transsion.com>
2025-06-26 10:21:00 +0300

    feat(main_activity): Add security flag and pin authentication

--- app/src/main/java/com/transsion/financialassistant/presentation/main_activity/MainActivity.kt
+++ app/src/main/java/com/transsion/financialassistant/presentation/main_activity/MainActivity.kt
@@ -2,20 +2,17 @@ package com.transsion.financialassistant.presentation.main_activity
 
 import android.os.Build
 import android.os.Bundle
-import android.view.Window
 import android.view.WindowManager
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.getValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ProcessLifecycleOwner
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
 import com.transsion.financialassistant.onboarding.navigation.OnboardingRoutes
 import com.transsion.financialassistant.presentation.navigation.FinancialAssistantNavHost
@@ -25,6 +22,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+
     @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,20 +31,18 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
 
         /**Flag to prevent app from capturing screenshots and blur the app content while in background*/
+        //FIXME -> Make this logic optional, user can allow or disallow
         this.window.setFlags(
             WindowManager.LayoutParams.FLAG_SECURE,
             WindowManager.LayoutParams.FLAG_SECURE,
         )
+
         setContent {
             val viewmodel = hiltViewModel<MainViewModel>()
             val financialAssistantController = rememberNavController()
-            val requireAuth by viewmodel.requireAuth.collectAsStateWithLifecycle()
-            /**Detect when app returns to foreground
-             * Trigger auth when app resumes
-             * */
+
             DisposableEffect(Unit) {
                 val observer = object : DefaultLifecycleObserver {
-
 
                     override fun onResume(owner: LifecycleOwner) {
                         super.onResume(owner)
